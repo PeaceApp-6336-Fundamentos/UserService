606548f3e522384b87af793776fbce7ccd4eefbf
Bryan Espejo <u202213278@upc.edu.pe>
2025-10-08 12:14:35 -0500

    Initial commit - PeaceApp User bounded context

---
+++ src/main/java/com/upc/pre/peaceapp/user/PeaceappUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.upc.pre.peaceapp.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeaceappUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeaceappUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upc/pre/peaceapp/user/PeaceappUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.pre.peaceapp.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeaceappUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
